b81fe5c983b08217e1813bac76976402ce681acf
echezona <elvis.okafor@venturegardengroup.com>
2021-07-11 22:28:07 +0100

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useHistory
+} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Login from './Login';
+import NotFound from './NotFound';
 import "./App.css";
 
 function App() {
-  console.log({ log: `${process.env.REACT_APP_HOSPITAL_HOST}${logo}` });
+  const history = useHistory();
+  console.log({ history});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={`${process.env.REACT_APP_HOSPITAL_HOST}${logo}`}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Link to="/about">About</Link> &nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/redux">Home</Link>&nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/">Login</Link>&nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/rrrror">not found</Link>
+      
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/redux" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact component={NotFound} />
+          </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>NotFound</h1>
+    <strong>NotFound page</strong>
+  </div>
+);
+
+export default NotFound;
