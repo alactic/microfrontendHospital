378898e20826056649305fe27206a81be06f747a
echezona <elvis.okafor@venturegardengroup.com>
2021-07-11 01:38:46 +0100

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log({ log: `${process.env.REACT_APP_HOSPITAL_HOST}${logo}` });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={`${process.env.REACT_APP_HOSPITAL_HOST}${logo}`}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// render micro frontend function
+window.renderHospitalapp = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId)
+  );
+};
+
+// unmount micro frontend function
+window.unmountHospitalapp = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+// Mount to root if it is not a micro frontend
+if (!document.getElementById("Hospitalapp-container")) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
